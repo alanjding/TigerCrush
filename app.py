8c74b76c819d5a73b9c2b77b18e4fbef2520b922
Oleg Golev <ogolev@princeton.edu>
2020-05-13 20:30:20 -0400

    Added simple routing

--- app.py
+++ app.py
@@ -1,12 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template, make_response
+from sys import argv, stderr
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates", static_folder="static")
+
+# -----------------------------------------------------------------------
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/login')
+def login():
+    html = render_template("login.html")
+    return make_response(html)
+
+# -----------------------------------------------------------------------
+
+
+@app.route('/index')
+def index():
+    html = render_template("index.html")
+    return make_response(html)
+
+# -----------------------------------------------------------------------
 
 
 if __name__ == '__main__':
-    app.run()
+
+    if len(argv) != 2:
+        print("Usage: " + argv[0] + " [port]", file=stderr)
+        exit(1)
+
+    try:
+        ret = int(argv[1])
+    except:
+        print("Port must be an integer", file=stderr)
+        exit(1)
+
+    app.run(host="0.0.0.0", port=int(argv[1]))
+
+# -----------------------------------------------------------------------
