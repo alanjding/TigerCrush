a8858fe5f0de2040ff91c6f8ad0b85cb68960f6e
alanjding <47194098+alanjding@users.noreply.github.com>
2020-05-21 14:20:57 -0400

    reset database script

--- app.py
+++ app.py
@@ -8,9 +8,15 @@
 # -------------------------------------------------------------------------------
 
 from flask import *
-import os
 from sys import argv, stderr
 from db_functions import *
+import os
+import hashlib
+import random
+from base64 import b64encode
+from datetime import datetime
+import requests
+import json
 
 # -----------------------------------------------------------------------
 #                         APP AND DATABASE SETUP
@@ -121,6 +127,64 @@ def addCrushEndpoint():
 
     # TODO - this is just placeholder code!
     return redirect(url_for('index'))
+
+# -----------------------------------------------------------------------
+#                   DATABASE INITIALIZATION COMMANDS
+# -----------------------------------------------------------------------
+
+# resets the database such that it consists of just a list of students and no
+# crushes between any two students
+@app.cli.command(name='resetDB')
+def resetDB():
+    print('Deleting existing data... ', end='')
+
+    # drop all previous crush data
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
+    print('Done!')
+    print('Fetching students from TigerBook... ', end='')
+
+    # grab TigerBook data
+    students = getStudents()
+
+    print('Done!')
+    print('Populating database with student data... ', end='')
+
+    # create rows in the db for each student
+    for student in students:
+        netid = student['net_id']
+        name = student['full_name']
+        year = student['class_year']
+        addUser(netid, name, year)
+
+    print('Done!')
+
+# -------------------------------------------------------------------------------
+
+# helper function that returns a Python dict of data grabbed from TigerBook
+def getStudents():
+    url = 'https://tigerbook.herokuapp.com/api/v1/undergraduates'
+    created = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+    nonce = ''.join([random.choice(
+        '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+/') for
+        i in range(32)])
+    username = 'ajding'
+    password = 'cedda4cc61fe1c736aada24eda32f5ab'
+    # password = os.environ.get('TIGERBOOK_KEY')
+    generated_digest = b64encode(hashlib.sha256(
+        nonce.encode('utf-8') + created.encode('utf-8') + password.encode(
+            'utf-8')).digest()).decode()
+    headers = {
+        'Authorization': 'WSSE profile="UsernameToken"',
+        'X-WSSE': 'UsernameToken Username="%s", PasswordDigest="%s", Nonce="%s", Created="%s"' % (
+            username, generated_digest, b64encode(nonce.encode()).decode(),
+            created)
+    }
+
+    r = requests.get(url, headers=headers)
+    return json.loads(r.text)
 
 # -----------------------------------------------------------------------
 #                            MAIN METHOD
--- db_init.py
+++ db_init.py
@@ -16,8 +16,7 @@
 # -------------------------------------------------------------------------------
 # click is a library that enables us to run Python scripts on Heroku
 import click
-from flask.cli import with_appcontext
-from app import db
+from app import db, app
 import os
 import hashlib
 import random
@@ -28,59 +27,3 @@ import json
 from db_functions import addUser
 # -------------------------------------------------------------------------------
 
-# resets the database such that it consists of just a list of students and no
-# crushes between any two students
-@click.command(name='resetDB')
-@with_appcontext
-def resetDB():
-    print('Deleting existing data... ', end='')
-
-    # drop all previous crush data
-    db.drop_all()
-    db.create_all()
-    db.session.commit()
-
-    print('Done!')
-    print('Fetching students from TigerBook... ', end='')
-
-    # grab TigerBook data
-    students = getStudents()
-
-    print('Done!')
-    print('Populating database with student data... ', end='')
-
-    # create rows in the db for each student
-    for student in students:
-        netid = student['net_id']
-        name = student['full_name']
-        year = student['class_year']
-        addUser(netid, name, year)
-
-    print('Done!')
-
-# -------------------------------------------------------------------------------
-
-# helper function that returns a Python dict of data grabbed from TigerBook
-def getStudents():
-    url = 'https://tigerbook.herokuapp.com/api/v1/undergraduates'
-    created = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
-    nonce = ''.join([random.choice(
-        '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+/') for
-        i in range(32)])
-    username = 'ajding'
-    password = 'cedda4cc61fe1c736aada24eda32f5ab'
-    # password = os.environ.get('TIGERBOOK_KEY')
-    generated_digest = b64encode(hashlib.sha256(
-        nonce.encode('utf-8') + created.encode('utf-8') + password.encode(
-            'utf-8')).digest()).decode()
-    headers = {
-        'Authorization': 'WSSE profile="UsernameToken"',
-        'X-WSSE': 'UsernameToken Username="%s", PasswordDigest="%s", Nonce="%s", Created="%s"' % (
-            username, generated_digest, b64encode(nonce.encode()).decode(),
-            created)
-    }
-
-    r = requests.get(url, headers=headers)
-    return json.loads(r.text)
-
-# -------------------------------------------------------------------------------
